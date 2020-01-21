cbf0153445d95f77afbaa265ecb2233777dd4252
Pavel Maslov <pavel.masloff@gmail.com>
2020-01-21 10:12:23 +0100

    small fixes

--- public/worker.js
+++ public/worker.js
@@ -1,7 +1,9 @@
 importScripts("https://cdn.jsdelivr.net/npm/jsqr@1.2.0/dist/jsQR.min.js");
 
-onmessage = function (event) {
+self.addEventListener('message', event => {
+  const { eventType, eventData, eventId } = event.data;
+  console.log(eventType);
   const {data, width, height} = event.data;
   const code = jsQR(data, width, height, { inversionAttempts: "dontInvert" });
   if (code != null) postMessage(code);
-};
+});
