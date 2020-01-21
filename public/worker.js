ccfce6da56c8790d3f3ab613dcb73b9ecf8410d7
Pavel Maslov <pavel.masloff@gmail.com>
2020-01-21 10:14:33 +0100

    bug fix

--- public/worker.js
+++ public/worker.js
@@ -2,7 +2,6 @@ importScripts("https://cdn.jsdelivr.net/npm/jsqr@1.2.0/dist/jsQR.min.js");
 
 self.addEventListener('message', event => {
   const { eventType, eventData, eventId } = event.data;
-  console.log(eventType);
   const {data, width, height} = event.data;
   const code = jsQR(data, width, height, { inversionAttempts: "dontInvert" });
   if (code != null) postMessage(code);
