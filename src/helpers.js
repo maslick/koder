adcc68e36aa35eddad8e347654d22c015d7f2864
Pavel Maslov <pavel.maslov@tretton37.com>
2021-02-20 21:52:28 +0100

    fixing

--- src/helpers.js
+++ src/helpers.js
@@ -46,7 +46,7 @@ const parseUpnQr = (obj) => {
 };
 
 const isUrl = (url) => {
-  const expression = /[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)?/gi;
+  const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)/;
   const regex = new RegExp(expression);
   return !!url.match(regex);
 };
