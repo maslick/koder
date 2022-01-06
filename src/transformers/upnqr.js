7472aaa400dc074568924341341f60e6bc6b6d35
Pavel Maslov <pavel.maslov@tretton37.com>
2022-01-06 12:04:39 +0200

    refactoring

--- src/transformers/upnqr.js
+++ src/transformers/upnqr.js
@@ -1,4 +1,4 @@
-import {CODE_TYPE, formatUpnQr} from "../helpers";
+import {CODE_TYPE} from "../helpers";
 import {decode} from "upnqr";
 
 class Upnqr {
@@ -14,7 +14,7 @@ class Upnqr {
 
   transform(raw) {
     try {
-      return formatUpnQr(decode(raw));
+      return this.formatUpnQr(decode(raw));
     }
     catch (e) {
       console.log(e);
@@ -24,6 +24,33 @@ class Upnqr {
   static buttonCaption() {
     return "UPNQR";
   }
+
+  formatUpnQr(obj){
+    let res = "";
+    let rok = "";
+    let date = obj.rok_placila;
+    if (date != null) {
+      console.log("Date: " + date);
+      const d = date.getDate();
+      const m = date.getMonth() + 1;
+      const y = date.getFullYear();
+      rok = `${d}.${m}.${y}`;
+    }
+
+    if (obj.ime_placnika != null) res += `Plačnik: ${obj.ime_placnika}\n`;
+    if (obj.ulica_placnika != null) res += `Naslov: ${obj.ulica_placnika}\n`;
+    if (obj.kraj_placnika != null) res += `Kraj: ${obj.kraj_placnika}\n\n`;
+    if (obj.znesek != null) res += `Znesek: ${obj.znesek} EUR\n`;
+    if (obj.koda_namena != null) res += `Koda namena: ${obj.koda_namena}\n`;
+    if (obj.namen_placila != null) res += `Namen: ${obj.namen_placila}\n`;
+    if (rok.length > 0) res += `Rok plačila: ${rok}\n`;
+    if (obj.IBAN_prejemnika != null) res += `IBAN: ${obj.IBAN_prejemnika}\n`;
+    if (obj.nujno != null) res += `Nujno: ${obj.nujno ? "da" : "ne"}\n\n`;
+    if (obj.referenca_prejemnika != null) res += `Referenca: ${obj.referenca_prejemnika}\n`;
+    if (obj.ulica_prejemnika != null) res += `Naslov: ${obj.ulica_prejemnika}\n`;
+    if (obj.kraj_prejemnika != null) res += `Kraj: ${obj.kraj_prejemnika}`;
+    return res;
+  };
 }
 
 export {Upnqr};
