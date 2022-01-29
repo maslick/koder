864b7b294951b4da3eef37f3780f2ed6866e53bb
Pavel Maslov <pavel.maslov@tretton37.com>
2022-01-29 19:04:01 +0200

    remove typescript dependency

--- config-overrides.js
+++ config-overrides.js
@@ -1,12 +1,10 @@
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  console.log('override!');
-
   config.resolve.fallback = {
     stream: require.resolve('stream-browserify'),
     buffer: require.resolve('buffer'),
-    util: require.resolve('util/'),
+    util: require.resolve('util'),
   };
   config.plugins.push(
     new webpack.ProvidePlugin({
