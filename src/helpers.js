c312d46af7ab22562d3059d8767319f2218d0d6e
Pavel Maslov <pavel.maslov@axfood.se>
2023-09-23 18:54:58 +0200

    use functional component; remove fps

--- src/helpers.js
+++ src/helpers.js
@@ -1,19 +1,20 @@
 const beep = (freq = 750, duration = 150, vol = 5) => {
-  try {
-    const context = window.audioContext;
-    const oscillator = context.createOscillator();
-    const gain = context.createGain();
-    oscillator.connect(gain);
-    oscillator.frequency.value = freq;
-    oscillator.type = "square";
-    gain.connect(context.destination);
-    gain.gain.value = vol * 0.01;
-    oscillator.start(context.currentTime);
-    oscillator.stop(context.currentTime + duration * 0.001);
-  } catch (e) {
-    console.warn("Sorry, Web Audio API is not supported by your browser");
-    console.warn(e.toString());
+  const AudioContext = window.AudioContext || window.webkitAudioContext || false;
+  if (!AudioContext) {
+    console.warn("Sorry, but Web Audio API is not supported by your browser");
+    return;
   }
+  const context = new AudioContext();
+  if (context == null) return;
+  const oscillator = context.createOscillator();
+  const gain = context.createGain();
+  oscillator.connect(gain);
+  oscillator.frequency.value = freq;
+  oscillator.type = "square";
+  gain.connect(context.destination);
+  gain.gain.value = vol * 0.01;
+  oscillator.start(context.currentTime);
+  oscillator.stop(context.currentTime + duration * 0.001);
 };
 
 const WORKER_TYPE = {
